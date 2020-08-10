adc5f3d248f7d7b718f472ae98706e08e0ac8d7a
soldi3rofwarz <josejehovanicastroperez@gmail.com>
2020-08-10 11:52:10 -0600

    componiendo el switch

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -12,16 +12,12 @@ namespace ConsoleApplication1
         {
 
 
-            int a, b, c, d;
+            int c;
 
-                Console.WriteLine("numero 1");
-                a = int.Parse(Console.ReadLine());
-                Console.WriteLine("numero 2");
-
-                b = int.Parse(Console.ReadLine());
-                Console.Clear();
-
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
+               
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.WriteLine("\t sumar 1 \n");
                 Console.WriteLine("\t restar 2 \n");
                 Console.WriteLine("\t multiplicar 3 \n");
@@ -31,47 +27,95 @@ namespace ConsoleApplication1
                 c = int.Parse(Console.ReadLine());
                 Console.Clear();
 
-               
+
 
                 switch (c)
                 {
                     case 1:
-                        d = a + b;
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine(a+" + " + b + " = " + d);
-                        Console.ReadKey();
-                    break;
+                        suma();
+                        break;
 
                     case 2:
-                        d = a - b;
-                        Console.ForegroundColor = ConsoleColor.Magenta;
-                        Console.WriteLine(a + " - " + b + " = " + d);
-                        Console.ReadKey();
-                    break;
+                        resta();
+                        break;
 
                     case 3:
-                        d = a * b;
-                        Console.ForegroundColor = ConsoleColor.Cyan;
-                        Console.WriteLine(a + " x " + b + " = " + d);
-                        Console.ReadKey();
-                    break;
+                        Multi();
+                        break;
 
                     case 4:
-                        d = a / b;
-                        Console.ForegroundColor = ConsoleColor.DarkYellow;
-                        Console.WriteLine(a + " / " + b + " = " + d);
-                        Console.ReadKey();
-                    break;
+                        div();
+                        break;
 
                     default:
                         Console.WriteLine("pendejo");
-                    break;
+                        break;
                 }
+            } while (c != 5);
 
 
+        }
+        public static void suma()
+        {
+            int a, b, d;
+            Console.WriteLine("numero 1");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("numero 2");
 
+            b = int.Parse(Console.ReadLine());
+            Console.Clear();
 
+            d = a + b;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(a + " + " + b + " = " + d);
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public static void resta()
+        {
+            int a, b, d;
+            Console.WriteLine("numero 1");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("numero 2");
 
+            b = int.Parse(Console.ReadLine());
+            Console.Clear();
+            d = a - b;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(a + " - " + b + " = " + d);
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public static void Multi()
+        {
+            int a, b, d;
+            Console.WriteLine("numero 1");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("numero 2");
+
+            b = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            d = a * b;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(a + " x " + b + " = " + d);
+            Console.ReadKey();
+        }
+
+        public static void div()
+        {
+            int a, b, d;
+            Console.WriteLine("numero 1");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("numero 2");
+
+            b = int.Parse(Console.ReadLine());
+            Console.Clear();
+            d = a / b;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(a + " / " + b + " = " + d);
+            Console.ReadKey();
         }
     }
 }
